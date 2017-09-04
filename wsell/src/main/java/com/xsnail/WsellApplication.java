bd432729e931a97c9cbb4b482fe16fe98aca3486
xsnail <xsnailslow@163.com>
2017-09-04 20:49:42 +0800

    todo

---
+++ wsell/src/main/java/com/xsnail/WsellApplication.java
@@ -0,0 +1,12 @@
+package com.xsnail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsellApplication.class, args);
+	}
+}
---
+++ wsell/src/main/java/com/xsnail/dataobject/ProductCategory.java
@@ -0,0 +1,12 @@
+package com.xsnail.dataobject;
+
+/**
+ * Created by Administrator on 2017/8/28 0028.
+ */
+public class ProductCategory {
+    private Integer categoryId;
+
+    public static void main(String[] args) {
+        System.out.println(Integer.parseInt("0909"));
+    }
+}
